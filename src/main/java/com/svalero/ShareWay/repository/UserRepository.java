2fb3feb8119ba9a76a4cd191d5f8055d11667140
Borja Z <borzg2000@gmail.com>
2026-02-06 12:25:18 +0100

    cambios en user

--- src/main/java/com/svalero/ShareWay/repository/UserRepository.java
+++ src/main/java/com/svalero/ShareWay/repository/UserRepository.java
@@ -5,7 +5,10 @@ import com.svalero.ShareWay.domain.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
+    List<User> findAll();
 }
