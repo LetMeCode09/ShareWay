3f0fa05b6f7ba8fe61b6edbc036870607358dabe
Aritz Ontalvilla <aritzontal@gmail.com>
2026-02-06 15:34:24 +0100

    fix2

--- src/main/java/com/svalero/ShareWay/controller/ReservationController.java
+++ src/main/java/com/svalero/ShareWay/controller/ReservationController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/reservations")
 public class ReservationController {
 
     private final ReservationService reservationService;
