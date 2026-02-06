81a92873c3089ae79c3da6f606707d18b75150a6
Borja Z <borzg2000@gmail.com>
2026-02-06 11:46:28 +0100

    domain user completo

--- src/main/java/com/svalero/ShareWay/controller/UserController.java
+++ src/main/java/com/svalero/ShareWay/controller/UserController.java
@@ -1,4 +1,12 @@
 package com.svalero.ShareWay.controller;
 
+import com.svalero.ShareWay.service.ReservationService;
+import com.svalero.ShareWay.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
 }
